bcf3dae53de36724141ef035d33f817de7505265
binyam tegene <binyampowerhc@gmail.com>
2022-05-31 12:44:52 -0700

    Have done Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for a in range(100):
-    if a != 99:
-        print("{:d}{:d}".format(a // 10, 0 % 10), end=", ")
+for b in range(0, 100):
+    if (b < 99):
+        print("{:02d}".format(b), end=', ')
     else:
-        print("{}".format(i))
-    
+        print("{:02d}".format(b))
