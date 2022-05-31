5bbf65db8dd5923e135be12a8075d9f5be90b708
binyam tegene <binyampowerhc@gmail.com>
2022-05-31 10:34:21 -0700

    Have done Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,3 @@ elif val == 0:
     print(f"Last digit of {number:d} is {val:d} and is 0")
 elif val < 6 and val != 0:
     print(f"Last digit of {number:d} is {val:d} and is less than 6 and not 0")
-
