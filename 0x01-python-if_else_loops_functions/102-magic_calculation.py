71b137639f5b98bfb21a05b8766812ce380c345e
binyam tegene <binyampowerhc@gmail.com>
2022-06-05 10:43:28 -0700

    Have done Task 102 Advanced

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-#import dis
-
-def magic_calculation(a, b, c):
-    if a < b
-        return (c)
-    elif c > b:
-        return (a + b)
-    return (a * b - c)
-# dis.dis(magic_calculation)
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    return sub(a, b)
