b8d6f661baf38a72c469b850bd7ae89e6e051112
binyam <binyampowerhc@gmail.com>
2022-06-10 13:48:37 +0300

    Have done Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-    }
+def roman_value(prmCharacter):
+    roman_list = [('I', 1), ('V', 5), ('X', 10),
+                  ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
+    for item in roman_list:
+        character, value = item
+        if (prmCharacter is character):
+            return value
+    return None
 
-    reuslt = 0
-    temp = list(roman_string)
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx +1] == 'V':
-                    temp [idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx +2])]
-            except IndexError:
-                pass
-            idx += 1
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
+
+def next_value(prmString, prmIndex):
+    if prmIndex + 1 < len(prmString):
+        return roman_value(prmString[prmIndex + 1])
+    else:
+        return None
+
+
+def roman_to_int(roman_string):
+    result = 0
+
+    if (roman_string is None or isinstance(roman_string, str) is False):
+        return result
+
+    enum = enumerate(roman_string)
+    for index, character in enum:
+        currentValue = roman_value(character)
+        nextValue = next_value(roman_string, index)
+        if nextValue is None or currentValue >= nextValue:
+            result += currentValue
+        else:
+            result += (nextValue - currentValue)
+            next(enum)
     return result
